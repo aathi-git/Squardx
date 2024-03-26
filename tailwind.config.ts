f074b399a689c25ce4b2ee5c1d6865d3eca4db88
Aathish <63100842+aathi-git@users.noreply.github.com>
2024-03-27 00:13:20 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,11 +19,11 @@ const config: Config = {
       keyframes: {
         spotlight: {
           "0%": {
-            opacity: 0,
+            opacity: "0", // Convert opacity value to string
             transform: "translate(-72%, -62%) scale(0.5)",
           },
           "100%": {
-            opacity: 1,
+            opacity: "1", // Convert opacity value to string
             transform: "translate(-50%,-40%) scale(1)",
           },
         },
